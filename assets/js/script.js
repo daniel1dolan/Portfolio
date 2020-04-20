dd51353e37b2fc83469a7939c60fb3252da89c17
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-04-20 15:28:18 -0500

    added veggies gone wild

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ var options = {
     "<strong>I'm</strong> a web developer.",
     "<strong>I</strong> love the outdoors.",
     "<strong>I</strong> have a Psychology Degree.",
-    "<strong>I</strong> love learning."
+    "<strong>I</strong> love learning.",
   ],
   typeSpeed: 100,
-  loop: true
+  loop: true,
 };
 
 var typed = new Typed(".typed", options);
@@ -14,14 +14,14 @@ var typed = new Typed(".typed", options);
 var options = {
   strings: ["Software"],
   typeSpeed: 100,
-  loop: false
+  loop: false,
 };
 
 var typed1 = new Typed(".typed1", options);
 
 $(() => {
-  $(document).ready(function() {
-    $("a.scrollLink").click(function(event) {
+  $(document).ready(function () {
+    $("a.scrollLink").click(function (event) {
       event.preventDefault();
       $("html, body").animate(
         { scrollTop: $($(this).attr("href")).offset().top },
@@ -29,24 +29,28 @@ $(() => {
       );
     });
   });
-  $("img", ".gifSet1").hover(function(e) {
+  $("img", ".gifSet1").hover(function (e) {
     $(".preset-file1").toggle();
     $(".gif-file1").toggle();
   });
-  $("img", ".gifSet2").hover(function(e) {
+  $("img", ".gifSet2").hover(function (e) {
     $(".preset-file2").toggle();
     $(".gif-file2").toggle();
   });
-  $("img", ".gifSet3").hover(function(e) {
+  $("img", ".gifSet3").hover(function (e) {
     $(".preset-file3").toggle();
     $(".gif-file3").toggle();
   });
-  $("img", ".gifSet4").hover(function(e) {
+  $("img", ".gifSet4").hover(function (e) {
     $(".preset-file4").toggle();
     $(".gif-file4").toggle();
   });
-  $("img", ".gifSet5").hover(function(e) {
+  $("img", ".gifSet5").hover(function (e) {
     $(".preset-file5").toggle();
     $(".gif-file5").toggle();
+  });
+  $("img", ".gifSet6").hover(function (e) {
+    $(".preset-file6").toggle();
+    $(".gif-file6").toggle();
   });
 });
